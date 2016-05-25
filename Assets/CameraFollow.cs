a7687023fb762370e534952b521f429e71002808
David Wang <davidcxwang@hotmail.com>
2016-05-25 01:10:30 -1000

    Camera Networking

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject induceCamera;
+	// Update is called once per frame
+	void Update () {
+	    if(transform.position != induceCamera.transform.position)
+        {
+            transform.position = Vector3.Slerp(transform.position, induceCamera.transform.position, Time.deltaTime * 1.0f);
+        }
+	}
+}
---
+++ Assets/CameraShowing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CameraShowing : NetworkBehaviour {
+
+    public Camera frontCamera;
+    public Camera leftCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (isServer)
+        {
+            return;
+        }else
+        {
+            frontCamera.enabled = false;
+        }
+	}
+	
+}
